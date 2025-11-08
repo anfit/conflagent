4f780823674b39185a744cac5d2974de95685884
Jan Chimiak <jan.chimiak@gmail.com>
2025-11-08 18:01:58 +0100

    refactor: adopt BEM landing page styling

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -18,9 +18,6 @@ def test_landing_page_serves_static_content(client):
     response = client.get('/')
     assert response.status_code == 200
     html = response.data.decode('utf-8')
+    assert 'landing__hero-title' in html
     assert 'Welcome to Conflagent' in html
     assert 'View API Specification' in html
-    assert 'Conflagent' in html
-    assert 'v1.0.0' in html
-    assert 'Built 2024-05-01' in html
-    assert 'Commit abc1234' in html
